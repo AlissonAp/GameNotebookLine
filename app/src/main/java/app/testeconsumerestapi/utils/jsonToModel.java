7b1a38cab2aff446a2ba283acf3879ffce8fd89d
AlissonAp <alisson.allebrandtt@gmail.com>
2017-11-01 21:58:29 -0200

    Create methods for convert JSON to Model and Model to JSON

--- app/src/main/java/app/testeconsumerestapi/utils/jsonToModel.java
+++ app/src/main/java/app/testeconsumerestapi/utils/jsonToModel.java
@@ -4,6 +4,11 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import app.testeconsumerestapi.models.Missao;
+import app.testeconsumerestapi.models.Peca;
 import app.testeconsumerestapi.models.propriedadesPeca;
 import app.testeconsumerestapi.models.regrasMissao;
 
@@ -34,7 +39,27 @@ public class jsonToModel {
         }
     }
 
+    public List<Missao> MissoesFromJSON(String JSON){
 
+        Gson gson = new Gson();
+        try {
+            ArrayList<Missao> missoes  = gson.fromJson(jsonConverter(JSON), ArrayList.class);
+            return missoes;
+        }catch(Exception ex){
+            return new ArrayList<Missao>();
+        }
+    }
+
+    public List<Peca> PecasFromJSON(String JSON){
+
+        Gson gson = new Gson();
+        try {
+            ArrayList<Peca> pecas  = gson.fromJson(jsonConverter(JSON), ArrayList.class);
+            return pecas;
+        }catch(Exception ex){
+            return new ArrayList<Peca>();
+        }
+    }
 
     public JsonElement jsonConverter(String JSON){
 
